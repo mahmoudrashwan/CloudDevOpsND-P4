7f1c948631be3a758a5679f3d3525004f45acb7c
Mahmoud Rashwan <mahmoud.rashwan001@gmail.com>
2020-05-10 05:28:27 +0000

    tag image before push to dockerhub

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,12 +5,14 @@
 # Assumes that an image is built via `run_docker.sh`
 
 # Step 1:
+version=1.1
 dockerpath=mahmoudrashwan001/sklearn-ml-app
 
 # Step 2:
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 docker login --username=mahmoudrashwan001
+docker tag "${dockerpath}" "${dockerpath}:${version}"
 
 # Step 3:
 docker push mahmoudrashwan001/sklearn-ml-app
